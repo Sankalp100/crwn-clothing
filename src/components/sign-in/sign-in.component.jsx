4b3b1ae8f951d0a44bf227a2fb67f92c22e4fb90
Sankalp Gour <sankalpgour06@gmail.com>
2021-06-08 00:47:59 +0530

    redux-saga

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,74 +1,91 @@
-import React from "react";
+import React from 'react';
+import { connect } from 'react-redux';
 
-import FormInput from "../../components/form-input/form-input.component";
-import CustomButton from '../custom-button/custom-button.component'
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
-import { auth, signInWithGoogle } from '../../firebase/firebase.utils'
+import {
+  googleSignInStart,
+  emailSignInStart
+} from '../../redux/user/user.actions';
 
-import "./sign-in.style.scss";
+import {
+  SignInContainer,
+  SignInTitle,
+  ButtonsBarContainer
+} from './sign-in.style';
 
 class SignIn extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      email: "",
-      password: "",
+      email: '',
+      password: ''
     };
   }
 
   handleSubmit = async event => {
     event.preventDefault();
-
+    const { emailSignInStart } = this.props;
     const { email, password } = this.state;
 
-    try{
-      await auth.signInWithEmailAndPassword(email, password)
-      this.setState({ email: "", password: "" });
-    } catch (error) {
-      console.log(error)
-    }
-
-    
+    emailSignInStart(email, password);
   };
 
-  handleChange = (event) => {
+  handleChange = event => {
     const { value, name } = event.target;
 
     this.setState({ [name]: value });
   };
 
   render() {
+    const { googleSignInStart } = this.props;
     return (
-      <div className="sign-in">
-        <h2>I already have an account</h2>
-        <span>Sign with ur email and password</span>
+      <SignInContainer>
+        <SignInTitle>I already have an account</SignInTitle>
+        <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
           <FormInput
-            name="email"
-            type="email"
-            value={this.state.email}
+            name='email'
+            type='email'
             handleChange={this.handleChange}
-            label="email"
+            value={this.state.email}
+            label='email'
             required
           />
           <FormInput
-            name="password"
-            type="password"
+            name='password'
+            type='password'
             value={this.state.password}
             handleChange={this.handleChange}
-            label="password"
+            label='password'
             required
           />
-          <div className='buttons'>
-          <CustomButton type="submit" >SIGN IN</CustomButton>
-          <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>SIGN IN with Google</CustomButton>
-          </div>
+          <ButtonsBarContainer>
+            <CustomButton type='submit'> Sign in </CustomButton>
+            <CustomButton
+              type='button'
+              onClick={googleSignInStart}
+              isGoogleSignIn
+            >
+              Sign in with Google
+            </CustomButton>
+          </ButtonsBarContainer>
         </form>
-      </div>
+      </SignInContainer>
     );
   }
 }
 
-export default SignIn;
+const mapDispatchToProps = dispatch => ({
+  googleSignInStart: () => dispatch(googleSignInStart()),
+  emailSignInStart: (email, password) =>
+    dispatch(emailSignInStart({ email, password }))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(SignIn);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,50 +1,92 @@
-import firebase from 'firebase/app'
-import 'firebase/firestore'
-import 'firebase/auth'
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
-    apiKey: "AIzaSyBv6v1PDRcZhQSRE_DLMrotn-n0TWF6SmE",
-    authDomain: "crwn-db-e07ee.firebaseapp.com",
-    projectId: "crwn-db-e07ee",
-    storageBucket: "crwn-db-e07ee.appspot.com",
-    messagingSenderId: "329925642253",
-    appId: "1:329925642253:web:cc508d2646cdec6587ed44",
-    measurementId: "G-M65LJ1SJ08"
+  apiKey: "AIzaSyBv6v1PDRcZhQSRE_DLMrotn-n0TWF6SmE",
+  authDomain: "crwn-db-e07ee.firebaseapp.com",
+  projectId: "crwn-db-e07ee",
+  storageBucket: "crwn-db-e07ee.appspot.com",
+  messagingSenderId: "329925642253",
+  appId: "1:329925642253:web:cc508d2646cdec6587ed44",
+  measurementId: "G-M65LJ1SJ08",
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
   }
 
-  export const createUserProfileDocument = async (userAuth, additionalData) => {
-    if(!userAuth) return;
+  return userRef;
+};
 
-    const userRef = firestore.doc(`users/${userAuth.uid}`);
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
 
-      const snapShot = await userRef.get()
+  const batch = firestore.batch();
+  objectsToAdd.forEach((obj) => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
 
-      if(!snapShot.exists) {
-        const {displayName, email} = userAuth;
-        const createdAt = new Date();
+  return await batch.commit();
+};
 
-        try {
-          await userRef.set({
-            displayName,
-            email,
-            createdAt,
-            ...additionalData
-          })
-        } catch (error){
-          console.log(error.message)
-        }
-      }
+export const convertCollectionsSnapshotToMap = (collection) => {
+  const transformedCollection = collection.docs.map((docSnapshot) => {
+    const { title, items } = docSnapshot.data();
 
-      return userRef;
-  }
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: docSnapshot.id,
+      title,
+      items,
+    };
+  });
 
-  firebase.initializeApp(config)
+  return transformedCollection.reduce((accumlator, collection) => {
+    accumlator[collection.title.toLowerCase()] = collection;
+    return accumlator;
+  }, {});
+};
 
-  export const auth = firebase.auth();
-  export const firestore = firebase.firestore();
+firebase.initializeApp(config);
 
-  const provider = new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({ prompt: 'select_account'})
-  export const signInWithGoogle = () => auth.signInWithPopup(provider)
+export const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      unsubscribe();
+      resolve(userAuth);
+    }, reject);
+  });
+};
 
-  export default firebase 
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(googleProvider);
+
+export default firebase;
